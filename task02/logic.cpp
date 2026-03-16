9ede47a76ce8b71d08295bad9ef0071df02d6769
KrutalevichLev <krutaleviclev@gmail.com>
2026-03-16 18:28:20 +0300

    task 02 work

---
+++ task02/logic.cpp
@@ -0,0 +1,17 @@
+#include "logic.h"
+#include <ctime>
+
+string detect_fortune() {
+	srand(time(0));
+	string fortune = "All your hard work will soon pay off.";
+
+	switch (rand() % 5) {
+	case 0: fortune = "You will have good luck and overcome many hardships."; break;
+	case 1: fortune = "If we don't change, we don't grow."; break;
+	case 2: fortune = "Your creativity will lead you to success."; break;
+	case 3: fortune = "A new adventure is waiting for you."; break;
+	}
+
+	return fortune;
+}
+
---
+++ task02/main.cpp
@@ -0,0 +1,6 @@
+#include "logic.h"
+
+int main() {
+	cout << detect_fortune();
+	return 0;
+}
