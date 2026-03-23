da12b8bf3fec9066e28e88ed8e8bbfdb28c34317
KrutalevichLev <krutalevichlev@gmail.com>
2026-03-23 16:46:25 +0300

    Update logic.cpp

--- task02/logic.cpp
+++ task02/logic.cpp
@@ -5,7 +5,7 @@ string detect_fortune() {
 	srand(time(0));
 	string fortune = "All your hard work will soon pay off.";
 
-	switch (rand() % 5) {
+	switch (rand() % NUMBER_OF_PREDICTION) {
 	case 0: fortune = "You will have good luck and overcome many hardships."; break;
 	case 1: fortune = "If we don't change, we don't grow."; break;
 	case 2: fortune = "Your creativity will lead you to success."; break;
